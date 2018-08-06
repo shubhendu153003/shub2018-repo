c0bf7e67602c94a5ed1324d3cc0a6294e6aad331
shumal <shumal@DIN73003018.corp.capgemini.com>
2018-08-06 13:02:38 +0530

    printall added

--- Day1-git2/src/main/java/org/demo/MainClass.java
+++ Day1-git2/src/main/java/org/demo/MainClass.java
@@ -13,4 +13,8 @@ public static void greet()
 	System.out.println("Hello Greet");
 	System.out.println("Hello Greet 2");
 }
+public static void printall()
+{
+	System.out.println("I am in printall");
+}
 }
