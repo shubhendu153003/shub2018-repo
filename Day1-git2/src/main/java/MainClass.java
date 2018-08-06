bbdbcb30f34da0d09eeac98a7aa977dc45e1fa2d
shumal <shumal@DIN73003018.corp.capgemini.com>
2018-08-06 10:52:48 +0530

    commit3

--- Day1-git2/src/main/java/MainClass.java
+++ Day1-git2/src/main/java/MainClass.java
@@ -2,5 +2,11 @@
 public class MainClass {
 public static void main(String[] args) {
 	System.out.println("Hello java");
+	greet();
+}
+public static void greet()
+{
+	System.out.println("Hello Greet");
+	System.out.println("Hello Greet 2");
 }
 }
