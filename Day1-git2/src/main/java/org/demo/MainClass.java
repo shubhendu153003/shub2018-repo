a97279825599e4871ec365b03266c7ac3c89554d
shumal <shumal@DIN73003018.corp.capgemini.com>
2018-08-06 12:20:36 +0530

    added commit4

--- Day1-git2/src/main/java/org/demo/MainClass.java
+++ Day1-git2/src/main/java/org/demo/MainClass.java
@@ -2,10 +2,10 @@ package org.demo;
 
 public class MainClass {
 public static void main(String[] args) {
-	SalesCalss s=new SalesCalss();
+	
 	System.out.println("Hello java");
 	greet();
-	s.show();
+
 	
 }
 public static void greet()
